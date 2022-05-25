bce4685f0c49037dc040d6e87dc22b7b41e43deb
Ksenia <kalagaevaxenia@ya.ru>
2022-05-25 19:02:46 +0300

    server settings

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,29 @@
-const merge = require("webpack-merge");
+const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+
 module.exports = merge(common, {
+  // Set the mode to development or production
   mode: "development",
+
+  //   Control how source maps are generated
+  devtool: "inline-source-map",
+
+  //   Spin up a server for quick development
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, '/dist'),
+    open: true,
+    compress: true,
+    port: 3010,
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 });
