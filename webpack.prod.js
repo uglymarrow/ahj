f837f2c9e4b32f892d4a6bc0fdcc1564495ffbbc
Ksenia <kalagaevaxenia@ya.ru>
2022-05-25 19:07:50 +0300

    webpack.prod. fixes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,8 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
